788717659671efb863171c7568e9f03e941f0e8f
hhsecond <sherin@tensorwerk.com>
2019-09-09 13:14:18 +0530

    cors issue

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from hangarapi import api
+from flask_cors import CORS
+
 
 app = Flask('HangarAPI')
+CORS(app)
 
 app.add_url_rule('/repository', view_func=api.RepositoryAPI.as_view('repository_api'))
 app.add_url_rule('/arrayset', view_func=api.ArraysetAPI.as_view('arrayset_api'))
