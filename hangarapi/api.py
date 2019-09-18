f5c6df31fc38d5a5d2bb0bfb5cc01bad6e4e07ae
hhsecond <sherin@tensorwerk.com>
2019-09-18 16:51:52 +0530

    repository get and post

--- hangarapi/api.py
+++ hangarapi/api.py
@@ -1,33 +1,69 @@
 from flask.views import MethodView
 from flask import jsonify, request
 
-from .utils import get_date
+from .config import SCREEN_DIR
+from .utils import get_valid_repo, create_repo
 
+
+# TODO: Logging
 
 class RepositoryAPI(MethodView):
 
     def get(self):
-        ret = {
-                'data': [
-                    {
-                        'name': 'Project 83',
-                        'desc': 'Some random Desc',
-                        'last_commit_date': get_date(),
-                        'commit_count': 215,
-                        'branch_count': 2
-                    },
-                    {
-                        'name': 'Visionary',
-                        'desc': 'Some random Desc 2',
-                        'last_commit_date': get_date(),
-                        'commit_count': 21,
-                        'branch_count': 3
-                    }], 'success': True, 'message': ''}
+        data = []
+        for d in SCREEN_DIR.iterdir():
+            if not d.is_dir():
+                continue
+            repo = get_valid_repo(d)
+            if repo:
+                # TODO: Create a hangar interface for all hangar interactions
+                cmt_details = repo.log(return_contents=True)
+                top = cmt_details['order'][0]
+                cmt_time = cmt_details['specs'][top]['commit_time']
+                repo_details = {
+                    'name': d.stem,
+                    'desc': d.stem,
+                    'last_commit_date': cmt_time,
+                    'commit_count': len(cmt_details['order']),
+                    'branch_count': len(repo.list_branches())
+                }
+                data.append(repo_details)
+        if data:
+            ret = {'data': data, 'success': True, 'message': ''}
+        else:
+            ret = {'data': [], 'success': False, 'message': "Found no valid hangar repositories"}
         return jsonify(ret), 200
 
     def post(self):
-        ret = {'success': True, 'message': 'Created', 'data': []}
+        if not request.json:
+            message = "provide required parameters"
+            ret = {'success': False, 'message': message, 'data': []}
+            return jsonify(ret), 400
+        try:
+            repo_name = request.json['name']
+            desc = request.json['desc']
+            username = request.json['username']
+            email = request.json['email']
+        except KeyError:
+            message = "provide required parameters"
+            ret = {'success': False, 'message': message, 'data': []}
+            return jsonify(ret), 400
+        path = SCREEN_DIR.joinpath(repo_name)
+        if not path.exists():
+            try:
+                path.mkdir()
+            except Exception as e:
+                print(e)
+                message = "Unknown error while creating repo directory"
+                ret = {'success': False, 'message': message, 'data': []}
+                return jsonify(ret), 500
+        status, message = create_repo(path, username, email, desc)
+        ret = {'success': status, 'message': message, 'data': []}
         return jsonify(ret), 201
+
+    def delete(self):
+        # TODO: allow deletion of repo from UI
+        raise NotImplementedError
 
 
 class ArraysetAPI(MethodView):
@@ -65,4 +101,11 @@ class SampleAPI(MethodView):
                 'shape': [5, 4]
             }
             ret['data'].append(each)
+        return jsonify(ret), 200
+
+
+class VersionAPI(MethodView):
+
+    def get(self):
+        ret = {'success': True, 'message': '', 'data': []}
         return jsonify(ret), 200
--- hangarapi/utils.py
+++ hangarapi/utils.py
@@ -1,7 +1,24 @@
-import time
-import random
+import hangar
 
 
-def get_date():
-    sec_diff = random.randint(0, 1728000)
-    return time.time() - sec_diff
+def get_valid_repo(path):
+    try:
+        repo = hangar.Repository(path)
+        # TODO: fix hangar's version compatibility check
+        if repo.initialized and repo.version >= '0.3.0':
+            return repo
+    except RuntimeError:
+        pass
+    return None
+
+
+def create_repo(path, username, email, desc):
+    repo = hangar.Repository(path)
+    if repo.initialized:
+        return False, "Repository with same name exist"
+    # TODO: Allow remove old from webUI
+    try:
+        repo.init(username, email)
+    except Exception as e:
+        return False, str(e)
+    return True, "Repository created successfully"
